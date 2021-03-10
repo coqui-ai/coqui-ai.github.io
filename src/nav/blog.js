7a140b166defe49186cbdd87b1db93f5acd16e6a
kdavis-coqui <kdavis@alum.mit.edu>
2021-03-10 07:49:25 +0100

    Fixed part of issue #11 (Switched headers to Coqui)

--- src/nav/blog.js
+++ src/nav/blog.js
@@ -1,5 +1,5 @@
 /**
- * Copyright Zendesk, Inc.
+ * Copyright Coqui GmbH
  *
  * Use of this source code is governed under the Apache License, Version 2.0
  * found at http://www.apache.org/licenses/LICENSE-2.0.
--- utils/deploy.js
+++ utils/deploy.js
@@ -1,4 +1,9 @@
-#!/usr/bin/env node
+/**
+ * Copyright Coqui GmbH
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
 
 /**
  * Copyright Zendesk, Inc.
