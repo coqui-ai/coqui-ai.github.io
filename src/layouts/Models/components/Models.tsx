a55afac9b2f0747f66257142aff5ec5227b6bb8d
kdavis-coqui <kdavis@alum.mit.edu>
2021-05-03 15:41:10 +0200

    Added sorting

--- src/layouts/Models/components/Models.tsx
+++ src/layouts/Models/components/Models.tsx
@@ -13,7 +13,16 @@ import { Anchor } from '@zendeskgarden/react-buttons';
 import { graphql, Link, useStaticQuery } from 'gatsby';
 import { Pagination } from '@zendeskgarden/react-pagination';
 import { Tabs, TabList, Tab, TabPanel } from '@zendeskgarden/react-tabs';
-import { Body, Cell, Head, HeaderCell, HeaderRow, Row, Table } from '@zendeskgarden/react-tables';
+import {
+  Body,
+  Cell,
+  Head,
+  HeaderCell,
+  SortableCell,
+  HeaderRow,
+  Row,
+  Table
+} from '@zendeskgarden/react-tables';
 import { ReactComponent as DownloadIcon } from '@zendeskgarden/svg-icons/src/16/download-stroke.svg';
 
 const StyledTable = styled(Table)`
@@ -28,6 +37,8 @@ const capitalize = s => {
 
   return s.charAt(0).toUpperCase() + s.slice(1);
 };
+
+type Direction = 'asc' | 'desc' | undefined;
 
 interface IRow {
   name: string;
@@ -58,10 +69,64 @@ const createRow = (row: IRow, index: number) => (
   </Row>
 );
 
+const sortData = (
+  tableData: IRow[],
+  modelCardSort: Direction,
+  languageSort: Direction,
+  creatorSort: Direction,
+  sttEngineSort: Direction,
+  modelVersionSort: Direction
+) => {
+  if (!modelCardSort && !languageSort && !creatorSort && !sttEngineSort && !modelVersionSort) {
+    return tableData;
+  }
+
+  // Currently all engines are pinned to the same value "Coqui STT v0.9.3"
+  if (sttEngineSort) {
+    return tableData;
+  }
+
+  let field: 'name' | 'language' | 'sttVersion' | 'modelVersion' | 'tagName';
+  let sortValue: Direction;
+
+  if (modelCardSort) {
+    field = 'name';
+    sortValue = modelCardSort;
+  } else if (languageSort) {
+    field = 'language';
+    sortValue = languageSort;
+  } else if (creatorSort) {
+    field = 'tagName';
+    sortValue = creatorSort;
+  } else {
+    field = 'modelVersion';
+    sortValue = modelVersionSort;
+  }
+
+  return tableData.sort((a, b) => {
+    const aValue = creatorSort ? tagNameMap[a[field]][0] : a[field];
+    const bValue = creatorSort ? tagNameMap[b[field]][0] : b[field];
+
+    if (aValue > bValue) {
+      return sortValue === 'asc' ? 1 : -1;
+    } else if (aValue < bValue) {
+      return sortValue === 'asc' ? -1 : 1;
+    }
+
+    return 0;
+  });
+};
+
 export const Models: React.FC = () => {
   const pageSize = 15;
   const [currentPage, setCurrentPage] = useState(1);
   const [selectedTab, setSelectedTab] = useState('stt');
+
+  const [modelCardSort, setModelCardSort] = useState<Direction>();
+  const [languageSort, setLanguageSort] = useState<Direction>();
+  const [creatorSort, setCreatorSort] = useState<Direction>();
+  const [sttEngineSort, setSTTEngineSort] = useState<Direction>();
+  const [modelVersionSort, setModelVersionSort] = useState<Direction>();
 
   const rawRowData = useStaticQuery(
     graphql`
@@ -94,6 +159,8 @@ export const Models: React.FC = () => {
     })
   );
 
+  const [data, setData] = useState(rowData);
+
   return (
     <MinWidthLayout>
       <Tabs
@@ -115,24 +182,131 @@ export const Models: React.FC = () => {
             <StyledTable isReadOnly>
               <Head>
                 <HeaderRow>
-                  <HeaderCell width="20%">Model Card</HeaderCell>
-                  <HeaderCell>Language</HeaderCell>
-                  <HeaderCell>Creator</HeaderCell>
-                  <HeaderCell>STT Engine</HeaderCell>
-                  <HeaderCell>Model Version</HeaderCell>
+                  <SortableCell
+                    width="20%"
+                    onClick={() => {
+                      if (modelCardSort === 'asc') {
+                        setModelCardSort('desc');
+                      } else if (modelCardSort === 'desc') {
+                        setModelCardSort(undefined);
+                      } else {
+                        setModelCardSort('asc');
+                      }
+                      setLanguageSort(undefined);
+                      setCreatorSort(undefined);
+                      setSTTEngineSort(undefined);
+                      setModelVersionSort(undefined);
+                      setData(data);
+                    }}
+                    sort={modelCardSort}
+                  >
+                    Model Card
+                  </SortableCell>
+                  <SortableCell
+                    onClick={() => {
+                      if (languageSort === 'asc') {
+                        setLanguageSort('desc');
+                      } else if (languageSort === 'desc') {
+                        setLanguageSort(undefined);
+                      } else {
+                        setLanguageSort('asc');
+                      }
+                      setModelCardSort(undefined);
+                      setCreatorSort(undefined);
+                      setSTTEngineSort(undefined);
+                      setModelVersionSort(undefined);
+                      setData(data);
+                    }}
+                    sort={languageSort}
+                  >
+                    Language
+                  </SortableCell>
+                  <SortableCell
+                    onClick={() => {
+                      if (creatorSort === 'asc') {
+                        setCreatorSort('desc');
+                      } else if (creatorSort === 'desc') {
+                        setCreatorSort(undefined);
+                      } else {
+                        setCreatorSort('asc');
+                      }
+                      setModelCardSort(undefined);
+                      setLanguageSort(undefined);
+                      setSTTEngineSort(undefined);
+                      setModelVersionSort(undefined);
+                      setData(data);
+                    }}
+                    sort={creatorSort}
+                  >
+                    Creator
+                  </SortableCell>
+                  <SortableCell
+                    onClick={() => {
+                      if (sttEngineSort === 'asc') {
+                        setSTTEngineSort('desc');
+                      } else if (sttEngineSort === 'desc') {
+                        setSTTEngineSort(undefined);
+                      } else {
+                        setSTTEngineSort('asc');
+                      }
+                      setModelCardSort(undefined);
+                      setLanguageSort(undefined);
+                      setCreatorSort(undefined);
+                      setModelVersionSort(undefined);
+                      setData(data);
+                    }}
+                    sort={sttEngineSort}
+                  >
+                    STT Engine
+                  </SortableCell>
+                  <SortableCell
+                    onClick={() => {
+                      if (modelVersionSort === 'asc') {
+                        setModelVersionSort('desc');
+                      } else if (modelVersionSort === 'desc') {
+                        setModelVersionSort(undefined);
+                      } else {
+                        setModelVersionSort('asc');
+                      }
+                      setModelCardSort(undefined);
+                      setLanguageSort(undefined);
+                      setCreatorSort(undefined);
+                      setSTTEngineSort(undefined);
+                      setData(data);
+                    }}
+                    sort={modelVersionSort}
+                  >
+                    Model Version
+                  </SortableCell>
                   <HeaderCell width="10%">Download</HeaderCell>
                 </HeaderRow>
               </Head>
               <Body>
                 {currentPage === 1
-                  ? rowData.slice(currentPage - 1, pageSize).map(createRow)
-                  : rowData
+                  ? sortData(
+                      data.slice(),
+                      modelCardSort,
+                      languageSort,
+                      creatorSort,
+                      sttEngineSort,
+                      modelVersionSort
+                    )
+                      .slice(currentPage - 1, pageSize)
+                      .map(createRow)
+                  : sortData(
+                      data.slice(),
+                      modelCardSort,
+                      languageSort,
+                      creatorSort,
+                      sttEngineSort,
+                      modelVersionSort
+                    )
                       .slice(currentPage * pageSize - pageSize, currentPage * pageSize)
                       .map(createRow)}
               </Body>
             </StyledTable>
             <Pagination
-              totalPages={Math.ceil(rowData.length / pageSize)}
+              totalPages={Math.ceil(data.length / pageSize)}
               currentPage={currentPage}
               onChange={setCurrentPage}
             />
