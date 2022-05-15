a4c979428b4f09282ba9c6a147c5c66f73ef498c
Valentin Golev <v.golev@gmail.com>
2022-05-15 20:50:48 +0200

    finish the signup dance logic

--- src/layouts/Auth/components/RequireAuth.tsx
+++ src/layouts/Auth/components/RequireAuth.tsx
@@ -15,24 +15,24 @@ import { SignInForm } from './SignInForm';
 export const ProfileContext = React.createContext<Profile>(null);
 
 export const RequireAuth = ({ children }) => {
-  const { data, loading, error } = useProfile();
+  const { data: profile, loading, error } = useProfile();
 
   if (loading) return 'Loading...';
-  if (error || !data) {
+  if (error || !profile) {
     return <SignInForm />;
   } // TODO: error-specific behaviour?
 
-  console.log(data);
-
-  // if (!data.email_validated) {
+  // if (!profile.email_validated) {
   //   return <VerifyEmail />;
   // }
-  if (!data.terms_accepted) {
-    return <AcceptTerms />;
-  }
-  if (!data.personal_name || !data.organization_name) {
-    return <CompleteProfile />;
+
+  if (!profile.personal_name || !profile.organization_name) {
+    return <CompleteProfile profile={profile} />;
   }
 
-  return <ProfileContext.Provider value={data}>{children}</ProfileContext.Provider>;
+  if (!profile.terms_accepted) {
+    return <AcceptTerms />;
+  }
+
+  return <ProfileContext.Provider value={profile}>{children}</ProfileContext.Provider>;
 };
--- src/layouts/Auth/components/registration/CompleteProfile.tsx
+++ src/layouts/Auth/components/registration/CompleteProfile.tsx
@@ -6,7 +6,80 @@
  */
 
 import React from 'react';
-import { useQuery, gql } from '@apollo/client';
+import { Formik, Field, Form } from 'formik';
+import { useQuery, gql, useMutation } from '@apollo/client';
 import { RequireAuth, ProfileContext } from './RequireAuth';
 
-export const CompleteProfile: React.FC = () => 'Please complete profile';
+const UPDATE_PROFILE = gql`
+  mutation updateProfile($personal_name: String!, $organization_name: String!) {
+    updateProfile(personal_name: $personal_name, organization_name: $organization_name) {
+      errors {
+        field
+        errors
+      }
+      profile {
+        organization_name
+        personal_name
+        terms_accepted
+      }
+    }
+  }
+`;
+
+export const CompleteProfile = ({ profile }) => {
+  const [updateProfile, { data, loading, error }] = useMutation(UPDATE_PROFILE);
+
+  return (
+    <Formik
+      initialValues={{
+        personal_name: profile.personal_name,
+        organization_name: profile.organization_name
+      }}
+      validate={values => {
+        const errors = {};
+
+        ['personal_name', 'organization_name'].forEach(k => {
+          if (!values[k]) errors[k] = 'Required.';
+        });
+
+        return errors;
+      }}
+      onSubmit={({ personal_name, organization_name }, formik) =>
+        updateProfile({ variables: { personal_name, organization_name } }).then(({ data }) => {
+          if (data.errors) {
+            formik.setStatus(
+              Object.fromEntries(data.errors.map(({ field, errors }) => [field, errors.join(' ')]))
+            );
+          } else {
+            return data;
+          }
+        })
+      }
+    >
+      {({ errors, touched, status }) => (
+        <Form>
+          {status?.__all__ ? <div>{status.__all__}</div> : null}
+
+          <label htmlFor="personal_name">Enter your name here:</label>
+          <Field id="personal_name" name="personal_name" type="personal_name" placeholder="" />
+          {errors.personal_name && touched.personal_name ? <div>{errors.personal_name}</div> : null}
+          {status?.personal_name ? <div>{status.personal_name}</div> : null}
+
+          <label htmlFor="organization_name">Name of organization:</label>
+          <Field
+            id="organization_name"
+            name="organization_name"
+            type="organization_name"
+            placeholder=""
+          />
+          {errors.organization_name && touched.organization_name ? (
+            <div>{errors.organization_name}</div>
+          ) : null}
+          {status?.organization_name ? <div>{status.organization_name}</div> : null}
+
+          <button type="submit">Sign Up & Login</button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
