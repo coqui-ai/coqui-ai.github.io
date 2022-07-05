683500fd53be472ce41b79d0e0d96a008e2009b1
Lime or Lemon <onur@limeorlemon.com>
2022-07-05 23:12:16 +0300

    Allow adding first line to scene
    Perhaps we should direct the user to Script Manager instead, when available.

--- src/layouts/Editor/components/AudioManager.tsx
+++ src/layouts/Editor/components/AudioManager.tsx
@@ -8,7 +8,7 @@
 import React, { useState } from 'react';
 import { css } from 'styled-components';
 
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { Add as AddIcon } from 'iconsax-react';
 
 import { Button } from '@zendeskgarden/react-buttons';
@@ -18,6 +18,7 @@ import Breadcrumb from './Breadcrumb';
 import LineEditor from './LineEditor';
 import NewSceneModal from './NewSceneModal';
 import * as queries from './Queries';
+import * as mutations from './Mutations';
 import SceneDropdown from './SceneDropdown';
 
 const AudioManager = ({ projectId, sceneId }) => {
@@ -32,6 +33,29 @@ const AudioManager = ({ projectId, sceneId }) => {
   const { data: lines } = useQuery(queries.LINES, { variables: { scene_id: sceneId } });
   const { data: emotions } = useQuery(queries.EMOTIONS);
   const { data: speakers } = useQuery(queries.SPEAKERS);
+
+  const [createLine, { createdLine, creating, error }] = useMutation(mutations.CREATE_LINE, {
+    update: cache => {
+      cache.evict({
+        id: 'ROOT_QUERY',
+        fieldName: 'lines',
+        args: { scene_id: scene.id }
+      });
+    }
+  });
+
+  const newLine = () => {
+    createLine({
+      variables: {
+        scene_id: sceneId,
+        text: "New line",
+        speed: 1.0,
+        emotion_id: "7b56cb9e-735c-4c37-8540-6ad62c380155", // Neutral
+        speaker_id: "f536ac80-3068-40d8-9f62-9d3428cab6b9",
+        emotion_intensity: 1.0,
+      }
+    });
+  };
 
   return (
     <>
@@ -92,17 +116,31 @@ const AudioManager = ({ projectId, sceneId }) => {
           Share
         </Button>
       </div>
-      <ul css={css`margin: ${p => p.theme.space.base * 4}px;`}>
-        {lines?.lines?.map(line => (
-          <LineEditor
-            key={line.id}
-            scene={scene?.scene}
-            line={line}
-            speakers={speakers?.speakers}
-            emotions={emotions?.emotions}
-          />
-        ))}
-      </ul>
+      {lines?.lines?.length > 0 ? (
+        <ul css={css`margin: ${p => p.theme.space.base * 4}px;`}>
+          {lines?.lines?.map(line => (
+            <LineEditor
+              key={line.id}
+              scene={scene?.scene}
+              line={line}
+              speakers={speakers?.speakers}
+              emotions={emotions?.emotions}
+            />
+          ))}
+        </ul>
+      ) : (
+        <div css={css`margin: ${p => p.theme.space.base * 4}px;`}>
+          <Button
+            disabled={creating}
+            onClick={newLine}
+          >
+            <Button.StartIcon>
+              <AddIcon color="#ed8f1c" />
+            </Button.StartIcon>
+            Add New Line
+          </Button>
+        </div>
+      )}
     </>
   );
 };
