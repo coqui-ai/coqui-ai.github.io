afb3662159442ed09effe6e6c4e36b9ad30e61bc
Reuben Morais <reuben.morais@gmail.com>
2023-01-08 14:28:32 +0100

    Use <audio> element instead of ctor to fix build

--- src/layouts/Home/components/Voices.tsx
+++ src/layouts/Home/components/Voices.tsx
@@ -82,32 +82,41 @@ const Characteristics = styled.div`
 `;
 
 const PlayButton = ({ src }: { src: string }) => {
-  const audioPlayer = useRef<HTMLAudioElement>(new Audio(src)).current;
+  const audioPlayer = useRef<HTMLAudioElement>(null);
 
   const [isPlaying, setIsPlaying] = useState(false);
 
   const play = () => {
-    audioPlayer.currentTime = 0;
-    audioPlayer.play();
+    if (audioPlayer.current) {
+      audioPlayer.current.currentTime = 0;
+    }
+    audioPlayer.current?.play();
     setIsPlaying(true);
   };
 
   const stop = () => {
-    audioPlayer.pause();
-    audioPlayer.currentTime = 0;
+    audioPlayer.current?.pause();
+    if (audioPlayer.current) {
+      audioPlayer.current.currentTime = 0;
+    }
     setIsPlaying(false);
   };
 
-  audioPlayer.addEventListener('ended', stop);
+  audioPlayer.current?.addEventListener('ended', stop);
 
-  return !isPlaying ? (
-    <Button css="height: 54px;" isBasic onClick={play} role="button" title="Play">
-      <PlayCircle size="54" color="#ed8f1c" variant="Bold" />
-    </Button>
-  ) : (
-    <Button css="height: 54px;" isBasic onClick={stop} role="button" title="Stop">
-      <StopCircle size="54" color="#ed8f1c" variant="Bold" />
-    </Button>
+  return (
+    <div>
+      <audio src={src} ref={audioPlayer} />
+      {!isPlaying ? (
+        <Button css="height: 54px;" isBasic onClick={play} role="button" title="Play">
+          <PlayCircle size="54" color="#ed8f1c" variant="Bold" />
+        </Button>
+      ) : (
+        <Button css="height: 54px;" isBasic onClick={stop} role="button" title="Stop">
+          <StopCircle size="54" color="#ed8f1c" variant="Bold" />
+        </Button>
+      )}
+    </div>
   );
 };
 
