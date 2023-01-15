2d5c3f0df4ba9812c26215fdf89020e30a64f9dc
Reuben Morais <reuben.morais@gmail.com>
2023-01-15 16:35:19 +0100

    Fix alignment on Why Coqui section
    Fixes https://github.com/coqui-ai/coqui-ai.github.io/issues/428
    Fixes https://github.com/coqui-ai/coqui-ai.github.io/issues/426

--- src/layouts/Home/components/WhyCoquiInset.tsx
+++ src/layouts/Home/components/WhyCoquiInset.tsx
@@ -5,7 +5,6 @@
  * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-import { Col, Grid, Row } from '@zendeskgarden/react-grid';
 import { getColor } from '@zendeskgarden/react-theming';
 import { LG, MD } from '@zendeskgarden/react-typography';
 import { ReactComponent as ArrowStroke } from '@zendeskgarden/svg-icons/src/16/arrow-left-stroke.svg';
@@ -20,63 +19,41 @@ export const WhyCoquiInset: React.FC<{
   to: string;
 }> = ({ title, text, to }) => {
   return (
-    <Grid
-      gutters={false}
+    <div
       css={css`
-        margin-bottom: ${p => p.theme.space.xxl};
+        display: flex;
+        flex-direction: column;
+        padding: 33px;
         background-color: ${p => getColor('yellow', 100, p.theme)};
         border-radius: 4px;
+        width: 220px;
+        height: 280px;
+        color: #000;
       `}
     >
-      <div
-        css={css`
-          padding: 33px;
-        `}
-      >
-        <Row
-          alignItems="center"
-          css={css`
-            margin-bottom: 24px;
-          `}
-        >
-          <Col>
-            <LG isBold>{title}</LG>
-          </Col>
-        </Row>
-        <Row
-          alignItems="center"
-          css={css`
-            margin-bottom: ${p => p.theme.space.xl};
-            max-height: 60px;
-          `}
-        >
-          <Col>
-            <MD>{text}</MD>
-          </Col>
-        </Row>
-        <Row alignItems="center">
-          <Col>
-            <HorizontalRule />
-            <MD>
-              <Link
-                to={to}
-                css={css`
-                  color: ${p => getColor('kale', 500, p.theme)};
-                `}
-              >
-                Learn More{' '}
-                <ArrowStroke
-                  css={css`
-                    transform: scale(-1, 1);
-                    transform-origin: center;
-                    vertical-align: text-top;
-                  `}
-                />
-              </Link>
-            </MD>
-          </Col>
-        </Row>
+      <LG isBold>{title}</LG>
+      <MD css="margin-top: 24px;">{text}</MD>
+      <div css="flex: 1;" />
+      <div>
+        <HorizontalRule />
+        <MD>
+          <Link
+            to={to}
+            css={css`
+              color: ${p => getColor('kale', 500, p.theme)};
+            `}
+          >
+            Learn More{' '}
+            <ArrowStroke
+              css={css`
+                transform: scale(-1, 1);
+                transform-origin: center;
+                vertical-align: text-top;
+              `}
+            />
+          </Link>
+        </MD>
       </div>
-    </Grid>
+    </div>
   );
 };
