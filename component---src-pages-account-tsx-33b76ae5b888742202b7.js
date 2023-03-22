19d91662be13ce38588f7d2eb89f7fb0939b8250
coqui-machine-user <reference-architecture-machine-user@coqui.ai>
2023-03-22 09:26:55 +0000

    Updates

--- component---src-pages-account-tsx-33b76ae5b888742202b7.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{asXk:function(n,t,o){"use strict";o.d(t,"a",(function(){return c}));var a=o("q1tI");function c(){Object(a.useEffect)((function(){var n=window.location.pathname+window.location.search;window.location.replace("https://app.coqui.ai"+n)}))}},vCyp:function(n,t,o){"use strict";o.r(t);var a=o("asXk");t.default=function(){return Object(a.a)(),""}}}]);
-//# sourceMappingURL=component---src-pages-account-tsx-33b76ae5b888742202b7.js.map
