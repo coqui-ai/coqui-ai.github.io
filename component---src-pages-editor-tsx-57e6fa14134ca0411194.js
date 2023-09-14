d6927222a6921928278d28b32e7ed6064c323aa3
coqui-machine-user <reference-architecture-machine-user@coqui.ai>
2023-09-14 09:58:22 +0000

    Updates

---
+++ component---src-pages-editor-tsx-57e6fa14134ca0411194.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[44],{"6Jv0":function(n,t,o){"use strict";o.r(t);var a=o("asXk");t.default=function(){return Object(a.a)(),""}},asXk:function(n,t,o){"use strict";o.d(t,"a",(function(){return c}));var a=o("q1tI");function c(){Object(a.useEffect)((function(){var n=window.location.pathname+window.location.search;window.location.replace("https://app.coqui.ai"+n)}))}}}]);
+//# sourceMappingURL=component---src-pages-editor-tsx-57e6fa14134ca0411194.js.map
--- component---src-pages-signup-tsx-8ff5894ad3f22dce1d3c.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[67],{asXk:function(n,t,o){"use strict";o.d(t,"a",(function(){return c}));var a=o("q1tI");function c(){Object(a.useEffect)((function(){var n=window.location.pathname+window.location.search;window.location.replace("https://app.coqui.ai"+n)}))}},smJ5:function(n,t,o){"use strict";o.r(t);var a=o("asXk");t.default=function(){return Object(a.a)(),""}}}]);
-//# sourceMappingURL=component---src-pages-signup-tsx-8ff5894ad3f22dce1d3c.js.map
